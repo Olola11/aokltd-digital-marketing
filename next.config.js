d3162a24b77b7f254eec6ca0f653c2447c852368
Olola11 <100452107+Olola11@users.noreply.github.com>
2026-02-02 01:53:23 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,22 +4,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-```
-
-Click **"Commit changes"** twice.
-
----
-
-## **STEP 4: Add .gitignore file**
-
-Click **"Add file"** → **"Create new file"**
-
-Name it: **`.gitignore`**
-
-Paste this content:
-```
-node_modules
-.next
-out
-.env.local
-.env.*.local
